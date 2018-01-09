630a41f30cb7d7bddde6f1ace742cddcd13f752e
Sarah Stauber <sarahstauber@Sarahs-MacBook-Pro.local>
2018-01-09 17:33:41 -0500

    completed HW

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,18 @@ var crystal4;
 var counter;
 var wins = 0;
 var loss = 0;
-// var numberOptions = [];
+var imageCrystal1 = $("<img>");
+var imageCrystal2 = $("<img>");
+var imageCrystal3 = $("<img>");
+var imageCrystal4 = $("<img>");
 
 
 function startGame()
 {
 	
-// $(".crystal-image").remove();
 
-	targetNumber = Math.floor(Math.random() * 121) + 19;
+
+	targetNumber = Math.floor(Math.random() * 102) + 19;
 	console.log(targetNumber);
 	$("#targetNumber").text(targetNumber);
 
@@ -25,54 +28,61 @@ function startGame()
 	crystal3 = Math.floor(Math.random()*12) +1;
 	crystal4 = Math.floor(Math.random()*12) +1;
 
-	// numberOptions = [];
-	// numberOptions = [crystal1, crystal2, crystal3, crystal4];
-
-	// counter = 0;
-
-	// for (var i=0; i < numberOptions.length; i++){
-
-	// 	var imageCrystal = $("<img>");
-	// 	imageCrystal.addClass("crystal-image");
-	// 	imageCrystal.attr("src", "assets/images/crystal1.jpg");
-	// 	imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-	// 	$("#crystals").append(imageCrystal);
-	// }
-
-
-
-
-	// crystal1 = parseInt(crystal1);
 
 	counter = 0;
 	$("#score").text(counter);
-	// $(".crystals").empty();
-	var imageCrystal1 = $("<img>");
+	
 	imageCrystal1.addClass("crystal-image");
 	imageCrystal1.attr("src", "assets/images/crystal1.jpg");
 	imageCrystal1.attr("data-crystalvalue", crystal1);
-	// imageCrystal1.attr("height", "200px");
 	$(".crystals").append(imageCrystal1);
 
-	var imageCrystal2 = $("<img>");
+	
 	imageCrystal2.addClass("crystal-image");
 	imageCrystal2.attr("src", "assets/images/crystal2.jpg");
 	imageCrystal2.attr("data-crystalvalue", crystal2);
-	// imageCrystal2.attr("height", "200px");
 	$(".crystals").append(imageCrystal2);
 
-	var imageCrystal3 = $("<img>");
+	
 	imageCrystal3.addClass("crystal-image");
 	imageCrystal3.attr("src", "assets/images/crystal3.jpg");
 	imageCrystal3.attr("data-crystalvalue", crystal3);
-	// imageCrystal3.attr("height", "200px");
 	$(".crystals").append(imageCrystal3);
 
-	var imageCrystal4 = $("<img>");
+	
 	imageCrystal4.addClass("crystal-image");
 	imageCrystal4.attr("src", "assets/images/crystal4.jpg");
 	imageCrystal4.attr("data-crystalvalue", crystal4);
-	// imageCrystal4.attr("height", "200px");
+	$(".crystals").append(imageCrystal4);
+}
+
+
+function resetGame()
+{
+	
+	targetNumber = Math.floor(Math.random() * 102) + 19;
+	console.log(targetNumber);
+	$("#targetNumber").text(targetNumber);
+
+	crystal1 = Math.floor(Math.random()*12) +1;
+	crystal2 = Math.floor(Math.random()*12) +1;
+	crystal3 = Math.floor(Math.random()*12) +1;
+	crystal4 = Math.floor(Math.random()*12) +1;
+
+
+	counter = 0;
+	$("#score").text(counter);
+
+	imageCrystal1.attr("data-crystalvalue", crystal1);
+	$(".crystals").append(imageCrystal1);
+
+	imageCrystal2.attr("data-crystalvalue", crystal2);
+	$(".crystals").append(imageCrystal2);
+
+	imageCrystal3.attr("data-crystalvalue", crystal3);
+	$(".crystals").append(imageCrystal3);
+
+	imageCrystal4.attr("data-crystalvalue", crystal4);
 	$(".crystals").append(imageCrystal4);
 }
 
@@ -80,7 +90,6 @@ startGame();
 
 
 $(".crystal-image").on("click", function(){
-
 	var crystalValue = ($(this).attr("data-crystalvalue"));
 	crystalValue = parseInt(crystalValue);
 	counter += crystalValue;
@@ -91,17 +100,14 @@ $(".crystal-image").on("click", function(){
 		alert("you win!");
 		wins++;
 		$("#wins").text(wins);
-		// $(".crystal-image").remove();
-		startGame();
+		resetGame();
 	}
 	else if(counter > targetNumber){
 		alert("you lose!");
 		loss++;
 		$("#loss").text(loss);
-		$(".crystal-image").remove();
-		startGame();
+		resetGame();
 	}
-
 });
 });
 
